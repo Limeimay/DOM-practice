bc631395bd76725adcd925628c5dbcb3491c43d2
limeimay <2765676012@qq.com>
2018-06-01 21:36:17 +0800

    first commit

---
+++ DOM综合示例/script/global.js
@@ -0,0 +1,43 @@
+// JavaScript source code
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if(typeof window.onload!='function'){
+        window.onload=func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement)
+    } else {
+        parent.insertBefore(newElement,targetElement.nextSibling)
+    }
+}
+function addClass(element,value) {
+    if (!element.className) {
+        element.className = value
+    }
+    if (element.className) {
+        newClassName = element.className;
+        newClassName += '';
+        newClassName += value;
+        element.className = newClassName;
+    }
+}
+//function highlightPage() {
+//    var navs = document.getElementsByTagName(nav);
+//    var links = navs[0].getElementsByTagName(a);
+//    var linkurl;
+//    for (var i = 0; i < links.length; i++) {
+//        linkurl = links[i].getAttribute("href");
+//        if (window.location.href.indexOf(linkurl) = -1) {
+//            links[i].className = "here";
+//        }
+//    }
+//}
+//addLoadEvent(highlightPage);
